bd7524fc750f001c2daede2dd6be8ac8d7e1001c
xiaochuanyi <1104849478@qq.com>
2018-04-25 23:56:48 +0800

    后台页面查询功能完成

--- manager-web/src/main/java/mall/controller/ItemController.java
+++ manager-web/src/main/java/mall/controller/ItemController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import mall.common.pojo.EasyUIDataGridResult;
 import mall.pojo.TbItem;
 import mall.service.ItemService;
 
@@ -18,5 +19,11 @@ public class ItemController {
 	public TbItem getItemById(@PathVariable Long itemId){
 		TbItem tbItem = itemservice.getItemById(itemId);
 		return tbItem;
+	}
+	@RequestMapping("/item/list")
+	@ResponseBody
+	public EasyUIDataGridResult getItemList(Integer page,Integer rows){
+		EasyUIDataGridResult result = itemservice.getItemList(page, rows);
+		return result;
 	}
 }
--- manager/manager-interface/src/main/java/mall/service/ItemService.java
+++ manager/manager-interface/src/main/java/mall/service/ItemService.java
@@ -1,7 +1,9 @@
 package mall.service;
 
+import mall.common.pojo.EasyUIDataGridResult;
 import mall.pojo.TbItem;
 
 public interface ItemService {
 	TbItem getItemById(long itemid);
+	EasyUIDataGridResult getItemList(int page,int rows);
 }
