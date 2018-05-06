94053114e0194c978fc2e6ca50c3a914f6b97480
xiaochuanyi <1104849478@qq.com>
2018-05-06 22:29:23 +0800

    搜索商品的缓冲实现和freemarker的实现

--- manager/manager-interface/src/main/java/mall/service/ItemService.java
+++ manager/manager-interface/src/main/java/mall/service/ItemService.java
@@ -3,10 +3,12 @@ package mall.service;
 import mall.commom.utils.E3Result;
 import mall.common.pojo.EasyUIDataGridResult;
 import mall.pojo.TbItem;
+import mall.pojo.TbItemDesc;
 
 public interface ItemService {
 	TbItem getItemById(long itemid);
 	EasyUIDataGridResult getItemList(int page,int rows);
 	E3Result addItem(TbItem item,String desc);
 	E3Result deleteItem(long ids);
+	TbItemDesc getItemDescById(long itemId);
 }
--- manager/manager-service/src/main/java/mall/service/impl/ItemServiceImpl.java
+++ manager/manager-service/src/main/java/mall/service/impl/ItemServiceImpl.java
@@ -10,7 +10,9 @@ import javax.jms.Message;
 import javax.jms.Session;
 import javax.jms.TextMessage;
 
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.jms.core.JmsTemplate;
 import org.springframework.jms.core.MessageCreator;
 import org.springframework.stereotype.Service;
@@ -18,14 +20,17 @@ import org.springframework.stereotype.Service;
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
 
+import jedis.JedisClient;
 import mall.commom.utils.E3Result;
 import mall.commom.utils.IDUtils;
+import mall.commom.utils.JsonUtils;
 import mall.common.pojo.EasyUIDataGridResult;
 import mall.mapper.TbItemDescMapper;
 import mall.mapper.TbItemMapper;
 import mall.pojo.TbItem;
 import mall.pojo.TbItemDesc;
 import mall.pojo.TbItemExample;
+import mall.pojo.TbItemExample.Criteria;
 import mall.service.ItemService;
 @Service
 public class ItemServiceImpl implements ItemService {
@@ -37,9 +42,44 @@ public class ItemServiceImpl implements ItemService {
 	private JmsTemplate jmsTemplate;
 	@Resource
 	private Destination topicDestination;
+	@Autowired
+	private JedisClient jedisClient;
+	@Value("${REDIS_ITEM_PRE}")
+	private String REDIS_ITEM_PRE;
+	@Value("${ITEM_CACHE_EXPIRE}")
+	private Integer ITEM_CACHE_EXPIRE;
 	public TbItem getItemById(long itemId) {
-		TbItem item = itemMapper.selectByPrimaryKey(itemId);
-		return item;
+		//查询缓存
+	try {
+		String json = jedisClient.get(REDIS_ITEM_PRE + ":" + itemId + ":BASE");
+		if(StringUtils.isNotBlank(json)) {
+		TbItem tbItem = JsonUtils.jsonToPojo(json, TbItem.class);
+		return tbItem;
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			}
+		//缓存中没有，查询数据库
+		//根据主键查询
+		//TbItem tbItem = itemMapper.selectByPrimaryKey(itemId);
+		TbItemExample example = new TbItemExample();
+		Criteria criteria = example.createCriteria();
+		//设置查询条件
+		criteria.andIdEqualTo(itemId);
+		//执行查询
+		List<TbItem> list = itemMapper.selectByExample(example);
+		if (list != null && list.size() > 0) {
+			//把结果添加到缓存
+		try {
+			jedisClient.set(REDIS_ITEM_PRE + ":" + itemId + ":BASE", JsonUtils.objectToJson(list.get(0)));
+			//设置过期时间
+			jedisClient.expire(REDIS_ITEM_PRE + ":" + itemId + ":BASE", ITEM_CACHE_EXPIRE);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			return list.get(0);
+		}
+		return null;
 	}
 	@Override
 	public EasyUIDataGridResult getItemList(int page, int rows) {
@@ -93,6 +133,29 @@ public class ItemServiceImpl implements ItemService {
 	public E3Result deleteItem(long ids) {
 		itemMapper.deleteByPrimaryKey(ids);
 		return E3Result.ok();
+	}
+	@Override
+	public TbItemDesc getItemDescById(long itemId) {
+		//查询缓存
+		try {
+			String json = jedisClient.get(REDIS_ITEM_PRE + ":" + itemId + ":DESC");
+			if(StringUtils.isNotBlank(json)) {
+				TbItemDesc tbItemDesc = JsonUtils.jsonToPojo(json, TbItemDesc.class);
+				return tbItemDesc;
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		TbItemDesc itemDesc = itemDescMapper.selectByPrimaryKey(itemId);
+		//把结果添加到缓存
+		try {
+			jedisClient.set(REDIS_ITEM_PRE + ":" + itemId + ":DESC", JsonUtils.objectToJson(itemDesc));
+			//设置过期时间
+			jedisClient.expire(REDIS_ITEM_PRE + ":" + itemId + ":DESC", ITEM_CACHE_EXPIRE);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return itemDesc;
 	}
 	
 }
