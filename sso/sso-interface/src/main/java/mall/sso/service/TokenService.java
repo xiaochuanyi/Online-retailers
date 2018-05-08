85ed30a29cb946b515274af2d7249ce6969e6471
xiaochuanyi <1104849478@qq.com>
2018-05-08 22:48:23 +0800

    登录及登录显示用户功能实现

---
+++ sso/sso-interface/src/main/java/mall/sso/service/TokenService.java
@@ -0,0 +1,14 @@
+package mall.sso.service;
+
+import mall.commom.utils.E3Result;
+
+/**
+ * 根据token查询用户信息
+ * <p>Title: TokenService</p>
+ * <p>Description: </p>
+ * <p>Company: www.itcast.cn</p> 
+ * @version 1.0
+ */
+public interface TokenService {
+	E3Result getUserByToken(String token);
+}
